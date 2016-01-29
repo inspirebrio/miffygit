16c7ba7af1d49b28e9d5a429d98896c617ec6bcf
sameer <sameersharma@innotical.com>
2016-01-29 18:25:32 +0530

    changeInImageField

--- shopApp/serializers.py
+++ shopApp/serializers.py
@@ -4,16 +4,16 @@ from .models import *
 
 class BannerAddPage_Serializer(serializers.ModelSerializer):
    # body_part = Body_partSerializer(many=True)
-    image_url = serializers.SerializerMethodField()
+    # image_url = serializers.ImageField()
     add_url = serializers.SerializerMethodField()
 
     class Meta:
         model = BannerAddPage
-        fields = ('ad_name','ad_description','image_url','add_url','banner_position')
+        fields = ('ad_name','ad_description','ad_banner1','add_url','banner_position')
 
-    def get_image_url(self,obj):
+    # def get_image_url(self,obj):
     	
-        return 'http:/'+obj.ad_banner1.path
+    #     return 'http:/'+obj.ad_banner1.path
     def get_add_url(self,obj):
     	
         return obj.ad_url1
