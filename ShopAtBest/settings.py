072bc6d040ed061267b30c5720da92d4beabf42f
sameer <sameersharma@innotical.com>
2016-02-10 19:12:36 +0530

    changeInSet

--- ShopAtBest/settings.py
+++ ShopAtBest/settings.py
@@ -64,23 +64,23 @@ CROS_ORIGIN_ALLOW_ALL=True
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'miffydb',
-#         'USER' : 'miffyuser',
-#         'PASSWORD' : 'miffy24x7',
-#         'HOST' :'miffypsql.ch79fqaqul7s.ap-southeast-1.rds.amazonaws.com',
-#         'PORT' : '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'miffydb',
+        'USER' : 'miffyuser',
+        'PASSWORD' : 'miffy24x7',
+        'HOST' :'miffypsql.ch79fqaqul7s.ap-southeast-1.rds.amazonaws.com',
+        'PORT' : '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
